0cf3a392ea4bd88faaa75b60b0340aba6fe26f6e
sku77 <ezrajohnasy@gmail.com>
2014-04-05 08:30:49 +0800

    Check Point. Signed-off-by:sku77 <ezrajohnasy@gmail.com>

--- wp-content/themes/blanktemplate/header.php
+++ wp-content/themes/blanktemplate/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
-	
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="index,follow" /> 
 	<?php } ?>
@@ -28,15 +28,20 @@
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
-	
-	<link rel="shortcut icon" href="/favicon.ico">
-	
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+        <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/assets/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.min.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/libraries/chosen/chosen.min.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/libraries/pictopro-outline/pictopro-outline.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/libraries/pictopro-normal/pictopro-normal.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/libraries/colorbox/colorbox.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/libraries/jslider/bin/jquery.slider.min.css" media="screen, projection">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap-datetimepicker.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen, projection">
+        <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:100,400,700,400italic,700italic" rel="stylesheet" type="text/css"  media="screen, projection">
 
 	<?php if ( is_singular() ) wp_enqueue_script('comment-reply'); ?>
-
 	<?php wp_head(); ?>
 </head>
 
