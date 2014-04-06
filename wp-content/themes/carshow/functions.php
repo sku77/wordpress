e7ac613a873c92109d5126ac96b3957f7c6b52f7
sku77 <ezrajohnasy@gmail.com>
2014-04-06 11:18:47 +0800

    Check Point. Signed-off-by:sku77 <ezrajohnasy@gmail.com>

--- wp-content/themes/carshow/functions.php
+++ wp-content/themes/carshow/functions.php
@@ -1,7 +1,6 @@
 <?php
 /** Start the engine */
 require_once( TEMPLATEPATH . '/lib/init.php' );
-
 /**
  * Functions
  *
@@ -9,13 +8,5 @@ require_once( TEMPLATEPATH . '/lib/init.php' );
  * @author       Ezra John <john@constantclick.com>
  * @copyright    Copyright (c) 2013, Ezra John
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
- *
- */
-/**
- * Theme Setup
- *
- * This setup function attaches all of the site-wide functions
- * to the correct actions and filters. All the functions themselves
- * are defined below this setup function.
  *
  */
