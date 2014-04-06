355ac8b72fad9b4a8c7fe53ddb409165b0bc7977
sku77 <ezrajohnasy@gmail.com>
2014-04-06 10:39:50 +0800

    Check Point. Signed-off-by:sku77 <ezrajohnasy@gmail.com>

--- wp-content/themes/carshow/front-page.php
+++ wp-content/themes/carshow/front-page.php
@@ -2,27 +2,27 @@
 /**
  * Front Page
  *
- * @package      Ezra John  
+ * @package      Ezra John
  * @author       Ezra John <john@constantclick.com>
  * @copyright    Copyright (c) 2013, Ezra John
  * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
  *
  */
-// Force full width 
+// Force full width
 add_filter('genesis_pre_get_option_site_layout', '__genesis_return_full_width_content');
 
-// Remove Page Title 
+// Remove Page Title
 remove_action('genesis_post_title', 'genesis_do_post_title');
 
-// Add Rotator 
+// Add Rotator
 add_action('genesis_after_header', 'be_home_rotator');
 
-// /** * Rotator * */ 
+// /** * Rotator * */
 function be_home_rotator() {
     do_action('home_rotator');
 }
 
-// Content Area remove_action( 'genesis_loop', 'genesis_do_loop' ); 
+// Content Area remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action('genesis_loop', 'be_home_loop');
 
 /** * Home Content Area * */
@@ -49,11 +49,25 @@ function be_home_loop() {
     echo '</div><!-- .content-middle -->';
     echo '<div class="content-right">';
     dynamic_sidebar('home-sidebar');
-    ?> 
-    <div class="events-links-entry"> <div class="entry-header"> <p class="col-1">Events</p> <p class="col-2">Popular Links</p> </div><!-- /entry-header --> <div class="entry-content"> 
+    ?>
+
+    <div class="events-links-entry"> <div class="entry-header">
+            <p class="col-1">Events</p> <p class="col-2">Popular Links</p>
+        </div><!-- /entry-header -->
+        <div class="entry-content">
             <?php
-            $args = array('post_type' => 'event', 'posts_per_page' => '5', 'orderby' => 'ASC', 'order' => 'meta_value_num', 'meta_query' => array(array('key' => 'be_events_manager_end_date', 'value' => time(), 'compare' => '>')));
+            $args = array(
+                'post_type' => 'event',
+                'posts_per_page' => '5',
+                'orderby' => 'ASC',
+                'order' => 'meta_value_num',
+                'meta_query' => array(
+                    array('key' => 'be_events_manager_end_date',
+                        'value' => time(),
+                        'compare' => '>')));
+
             $events = new WP_Query($args);
+
             if ($events->have_posts()): echo '<ul class="col-1">';
                 while ($events->have_posts()): $events->the_post();
                     echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
@@ -61,16 +75,26 @@ function be_home_loop() {
                 wp_reset_query();
                 echo '</ul>';
             endif;
-            ?> 
-            <ul class="col-2"> 
-                <?php
-                $args = array('limit' => -1, 'title_li' => '', 'category' => 2, 'categorize' => 0);
-                wp_list_bookmarks($args);
-                ?> 
-            </ul> <div class="cl"> </div> </div><!-- /entry-content --> </div><!-- /events-links --> </div><!-- .content-right --> 
-            <?php
-        }
+            ?>
 
-        genesis();
-        ?>
+            <ul class="col-2">
+                <?php
+                $args = array(
+                    'limit' => -1,
+                    'title_li' => '',
+                    'category' => 2,
+                    'categorize' => 0);
+                wp_list_bookmarks($args);
+                ?>
+            </ul>
+            <div class="cl">
+            </div>
+        </div><!-- /entry-content -->
+    </div><!-- /events-links -->
+    </div><!-- .content-right -->
+    <?php
+}
+
+genesis();
+?>
 
