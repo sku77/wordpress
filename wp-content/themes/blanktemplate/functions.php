c5bb47e16d556f262663e8c14ed8c7c70fb7eb4d
sku77 <ezrajohnasy@gmail.com>
2014-04-06 11:45:20 +0800

    Check Point. Signed-off-by:sku77 <ezrajohnasy@gmail.com>

--- wp-content/themes/blanktemplate/functions.php
+++ wp-content/themes/blanktemplate/functions.php
@@ -94,6 +94,7 @@ add_action('widgets_init', 'secondary_sidebar');
  * @param array $post_types An array of post type names that the templates be used by
  * @return array The array of post type names that the templates be used by
  * */
+
 function my_cpt_post_types($post_types) {
     $post_types[] = 'faq';
     $post_types[] = 'car';
